6e314059f8a1f30fc6df4b4851a0d984ca5930fc
Shahryar <shahryar.khalid@gmail.com>
2012-07-31 22:57:06 -0400

    made things work

--- PMP/Controllers/ProcessGroupsController.cs
+++ PMP/Controllers/ProcessGroupsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace PMP.Controllers
 {
@@ -13,6 +14,15 @@ namespace PMP.Controllers
 
         public ActionResult Index()
         {
+            //string xmlFile = @"\Content\ProcessGroups.xml";
+
+            //XDocument doc = XDocument.Load(xmlFile);
+            //XElement testCfg = XElement.Load(xmlFile);
+            //IEnumerable<XElement> test = doc.Elements();
+
+            //XElement child = testCfg.Element("DownloadedEmailIdList");
+
+            //IEnumerable<XElement> emailsList = child.Elements("i");
             return View();
         }
 

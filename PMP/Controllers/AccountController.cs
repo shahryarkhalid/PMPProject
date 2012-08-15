5600dec5d13aa5ef2a251f20feba9c238912529f
Shahryar <shahryar.khalid@gmail.com>
2012-08-15 13:32:40 -0400

    added ForgotPassword.cshtml and Print.cshtml

--- PMP/Controllers/AccountController.cs
+++ PMP/Controllers/AccountController.cs
@@ -149,6 +149,44 @@ namespace PMP.Controllers
         {
             return View();
         }
+        //
+        // GET: /Account/ForgotPassword
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/ForgotPassword
+
+        [HttpPost]
+        public ActionResult ForgotPassword(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
 
         #region Status Codes
         private static string ErrorCodeToString(MembershipCreateStatus createStatus)
